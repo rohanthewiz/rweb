ba6f2309598ead9992e9d8049f5f48d1249b210c
Rohan Allison <rohan.allison@cbre.com>
2025-02-11 14:01:12 -0600

    Chore: move proxy and SSE tests to their own files

--- proxy_test.go
+++ proxy_test.go
@@ -1,7 +1,6 @@
 package rweb_test
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -10,7 +9,6 @@ import (
 	"net/http"
 	"syscall"
 	"testing"
-	"time"
 
 	"git.akyoto.dev/go/assert"
 	"github.com/rohanthewiz/rweb"
@@ -138,93 +136,137 @@ func TestProxy(t *testing.T) {
 	_ = pxy.Run()
 }
 
-func TestSSE(t *testing.T) {
-	readyChan := make(chan struct{}, 1)
-	clientDone := make(chan struct{})
-
-	// Create event channel with buffer
-	eventsChan := make(chan any, 8)
-
-	s := rweb.NewServer(rweb.ServerOptions{
-		Verbose:   true,
-		ReadyChan: readyChan,
-		Address:   "localhost:",
+/*// TestProxySkewed tests proxying to one server by default and another server based on a prefix
+func TestProxySkewed(t *testing.T) {
+	// Init US Server
+	usTgtReadyChan := make(chan struct{}, 1)
+	usTgt := rweb.NewServer(rweb.ServerOptions{
+		Verbose: true, ReadyChan: usTgtReadyChan, Address: "localhost:", // run with high-order port
 	})
-	s.Get("/events", s.SSEHandler(eventsChan))
+	usTgt.Get("/", func(ctx rweb.Context) error {
+		return ctx.WriteString("Hi from US server root")
+	})
 
-	go func() { // Run our SSE client
-		defer close(clientDone)
-		defer syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+	usTgt.Get("/proxy-incoming", func(ctx rweb.Context) error {
+		return ctx.WriteHTML("<h1>Welcome to the US server landing!</h1>")
+	})
 
-		<-readyChan // wait for server
-
-		// Create client with timeout
-		client := &http.Client{
-			Timeout: 0, // Disable timeout for SSE connection
+	usTgt.Post("/proxy-incoming/status", func(ctx rweb.Context) error {
+		data := map[string]string{
+			"status": "success",
 		}
+		return ctx.WriteJSON(data)
+	})
 
-		resp, err := client.Get(fmt.Sprintf("http://127.0.0.1:%s/events", s.GetListenPort()))
+	// Init Euro Server
+	euTgtReadyChan := make(chan struct{}, 1)
+	euTgt := rweb.NewServer(rweb.ServerOptions{Verbose: true, ReadyChan: euTgtReadyChan, Address: "localhost:"})
+	euTgt.Post("/", func(ctx rweb.Context) error {
+		return ctx.WriteString("Hi from EU server root")
+	})
+
+	euTgt.Get("/proxy-incoming", func(ctx rweb.Context) error {
+		return ctx.WriteHTML("<h1>Welcome to the EU server landing!</h1>")
+	})
+
+	euTgt.Post("/proxy-incoming/status", func(ctx rweb.Context) error {
+		fv := ctx.Request().FormValue("abc")
+		return ctx.WriteString(ctx.Request().GetPostValue("def") + fv)
+	})
+
+	go func() {
+		_ = usTgt.Run()
+	}()
+
+	go func() {
+		_ = euTgt.Run()
+	}()
+
+	<-usTgtReadyChan // wait for US server
+	<-euTgtReadyChan // wait for EUR server
+
+	// Init Proxy
+	pxyReadyChan := make(chan struct{}, 1)
+	pxy := rweb.NewServer(rweb.ServerOptions{Verbose: true, ReadyChan: pxyReadyChan, Address: "localhost:"})
+
+	// e.g. curl http://localhost:8080/via-proxy/us/status
+	// 		- This will proxy to http://localhost:8081/usa/proxy-incoming/status
+	// e.g. curl http://localhost:8080/via-proxy/us
+	err := pxy.Proxy("/",
+		fmt.Sprintf("http://localhost:%s/proxy-incoming", usTgt.GetListenPort()), 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// e.g. curl http://localhost:8080/via-proxy/eu/status
+	err = pxy.Proxy("/eu",
+		fmt.Sprintf("http://localhost:%s/proxy-incoming", euTgt.GetListenPort()), 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pxy.Get("/admin*", func(ctx rweb.Context) error {
+		return ctx.WriteString("At proxy server admin")
+	})
+
+	go func() {
+		defer func() {
+			_ = syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+		}()
+
+		<-pxyReadyChan // wait for proxy
+
+		// Proxy to US server status
+		resp, err := http.Post(fmt.Sprintf("http://127.0.0.1:%s/status", pxy.GetListenPort()),
+			string(consts.BytFormData), bytes.NewReader([]byte("abc=123&def=456")))
 		assert.Nil(t, err)
 		assert.Equal(t, resp.Status, consts.OK200)
 
+		body, _ := io.ReadAll(resp.Body)
+		_ = resp.Body.Close()
+
+		respMap := map[string]string{}
+		_ = json.Unmarshal(body, &respMap)
+
+		if st, ok := respMap["status"]; ok {
+			assert.Equal(t, st, "success")
+		} else {
+			t.Error("Response map does not contain 'status'")
+		}
+
+		// Call a Non-proxied route
+		resp, err = http.Post(fmt.Sprintf("http://127.0.0.1:%s/admin/home", pxy.GetListenPort()),
+			string(consts.BytFormData), bytes.NewReader([]byte("abc=123&def=456")))
+		assert.Nil(t, err)
+		assert.Equal(t, resp.Status, consts.OK200)
+
+		body, _ = io.ReadAll(resp.Body)
+		_ = resp.Body.Close()
+		assert.Equal(t, string(body), "At proxy server admin")
+
+		// Proxy to US server root
+		resp, err = http.Get(fmt.Sprintf("http://127.0.0.1:%s", pxy.GetListenPort()))
+		assert.Nil(t, err)
+		assert.Equal(t, resp.Status, consts.OK200)
+		body, _ = io.ReadAll(resp.Body)
 		defer func() {
 			_ = resp.Body.Close()
 		}()
+		assert.Equal(t, string(body), "<h1>Welcome to the US server landing!</h1>")
 
-		// Verify headers
-		assert.Equal(t, resp.Header.Get("Content-Type"), "text/event-stream")
-		assert.Equal(t, resp.Header.Get("Cache-Control"), "no-cache")
-		assert.Equal(t, resp.Header.Get("Connection"), "keep-alive")
+		// Proxy to EU
+		resp, err = http.Post(fmt.Sprintf("http://127.0.0.1:%s/eu/status?abc=xyz", pxy.GetListenPort()),
+			string(consts.BytFormData), bytes.NewReader([]byte("abc=xyz&def=456")))
+		assert.Nil(t, err)
+		assert.Equal(t, resp.Status, consts.OK200)
 
-		// Read events with scanner
-		scanner := bufio.NewScanner(resp.Body)
-		eventsReceived := 0
-
-		// Start a timeout timer
-		timeout := time.After(10 * time.Second)
-
-		// Send events in background
-		go func() {
-			eventsChan <- "event 1"
-			eventsChan <- "event 2"
-			eventsChan <- "event 3"
-			// No need to wait as closed channel just means we won't add any more events.
-			// time.Sleep(100 * time.Millisecond)
-			close(eventsChan)
+		body, _ = io.ReadAll(resp.Body)
+		defer func() {
+			_ = resp.Body.Close()
 		}()
-
-		for eventsReceived < 3 {
-			select {
-			case <-timeout:
-				t.Error("Test timed out waiting for events")
-				return
-			default:
-				if !scanner.Scan() {
-					if err := scanner.Err(); err != nil {
-						t.Errorf("Scanner error: %v", err)
-					}
-					break
-				}
-				line := scanner.Text()
-				if line != "" {
-					eventsReceived++
-					expected := fmt.Sprintf("data: event %d", eventsReceived)
-					t.Logf("Received event: %s", line)
-					assert.Equal(t, line, expected)
-				}
-			}
-		}
-
-		assert.Equal(t, eventsReceived, 3)
+		assert.Equal(t, string(body), "456xyz")
 	}()
 
-	_ = s.Run()
-
-	// Wait for test completion or timeout
-	select {
-	case <-clientDone:
-		// Test completed normally
-	case <-time.After(15 * time.Second):
-		t.Fatal("Test did not complete within timeout")
-	}
+	_ = pxy.Run()
 }
+*/
